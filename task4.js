50b805833ffad9725923254622cb7612459c6d2b
Harisankar4612 <harisankar4612@gmail.com>
2024-11-06 09:33:48 +0530

    task4

---
+++ task4.js
@@ -0,0 +1,50 @@
+//age
+let age =23;
+if (age>=0 && age <12) {
+    console.log("Child");
+    
+}
+else if(age>=13 && age <19) {
+    console.log("Teenager");
+    
+}
+else if(age>=20 && age <35) {
+    console.log("Young Adult");
+    
+}
+else if(age>=36 && age <55) {
+    console.log("Adult");
+    
+}
+else if(age>=56) {
+    console.log("Senior");
+    
+}
+else{
+    console.log("Check your age");
+}
+
+//grade
+let mark=72;
+if (mark>=90 &&mark<100) {
+    console.log("you obtained A grade");
+}
+else if (mark>=80 &&mark<89) {
+    console.log("you obtained B grade");
+}
+else if (mark>=70 &&mark<79) {
+    console.log("you obtained C grade");
+}
+else if (mark>=60 &&mark<69) {
+    console.log("you obtained D grade");
+}
+else if (mark>=50 &&mark<59) {
+    console.log("you obtained E grade");
+}
+else if (mark>=50) {
+    console.log("you obtained F grade");
+}
+else{
+    console.log("check your mark Entered");
+    
+}
