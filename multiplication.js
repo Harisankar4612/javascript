47523e7fb0f58cb7520e19861e9669812e316f4e
Harisankar4612 <harisankar4612@gmail.com>
2024-11-06 13:22:22 +0530

    task4

---
+++ multiplication.js
@@ -0,0 +1,6 @@
+let number=2;
+let table="";
+for (let i = 1; i <=10; i++){
+    table+= ` ${number} * ${i} =${number * i}\n`;  
+}
+console.log(table);
---
+++ reverse.js
@@ -0,0 +1,6 @@
+let string="strix";
+let reversed="";
+for (let i = string.length-1; i >= 0; i--) {
+    reversed += string[i];
+}
+    console.log(reversed);
