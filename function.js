832a3376c6fcef987d358cee75f043f2377e297a
Harisankar4612 <harisankar4612@gmail.com>
2024-11-11 16:25:05 +0530

    task

---
+++ function.js
@@ -0,0 +1,18 @@
+function sum() {
+    let sums =5+8; 
+    console.log(sums); 
+}
+sum();
+
+function sum(a1,a2) {
+    let add=a1+a2;
+    console.log(add);
+    
+}
+sum(10,32);
+
+function sum(b1,b2) {
+    return b1+b2
+}
+let x=sum(5,7);
+console.log(x);
