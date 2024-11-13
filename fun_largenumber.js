a740f75b92d468a6c0081467f326292f164b11f4
Harisankar4612 <harisankar4612@gmail.com>
2024-11-13 09:24:36 +0530

    task

--- fun_largenumber.js
+++ fun_largenumber.js
@@ -1,5 +1,14 @@
-function largenumber(a, b, c) {
-    return Math.max(a, b, c);
+function findlargest(a, b, c) {
+    if (a >= b && a >= c) {
+        return a;
+    } else if (b >= a && b >= c) {
+        return b;
+    } else {
+        return c;
+    }
 }
-let n1 = 170, n2 = 125, n3 = 55;
-console.log("The largest number is: " + largenumber(n1, n2, n3));
+let num1 = 10;
+let num2 = 25;
+let num3 = 15;
+
+console.log("The largest number is: " + findlargest(num1, num2, num3)); 
